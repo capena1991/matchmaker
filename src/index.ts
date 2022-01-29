eae51870b7ef724bf05dcbfe69362b896b110f0f
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2022-01-29 13:52:52 -0500

    Prefer single quotes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-import { Client, Intents } from "discord.js"
+import { Client, Intents } from 'discord.js'
 
-import { token } from "./utils/config"
+import { token } from './utils/config'
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
-client.once("ready", () => {
+client.once('ready', () => {
   if (!client.user) {
-    throw "Logged in but no user."
+    throw 'Logged in but no user.'
   }
   console.log(`Logged in as ${client.user.tag}!`)
-  client.user.setPresence({ status: "online", activities: [{ name: "love in the air.", type: "WATCHING" }] })
+  client.user.setPresence({ status: 'online', activities: [{ name: 'love in the air.', type: 'WATCHING' }] })
 })
 
-// Login to Discord with your client's token
-client.login(token)
+client.login(token).catch(() => {
+  process.exit()
+})
